758a5cdad0b0513efaa5bc80857d16d11c576375
Blangish <rebox3@gmail.com>
2022-08-12 17:55:32 +0300

    Completed cat

--- s21_projects/SimpleBash/src/cat/s21_cat.c
+++ s21_projects/SimpleBash/src/cat/s21_cat.c
@@ -1,7 +1,6 @@
 #include "s21_cat_header.h"
 
-typedef struct options_t
-{
+typedef struct options_t {
     int b;
     int e;
     int n;
@@ -11,96 +10,89 @@ typedef struct options_t
 }opt;
 
 
-static struct option long_options[] =
-{
+static struct option long_options[] = {
     {"--number", 0, 0, 'n'},
     {"--squeeze-blank", 0, 0, 's'},
     {"--number-nonblank", 0, 0, 'b'},
     {0, 0, 0, 0}
 };
 
-void parser(int argc, char **argv, opt *options)
-{
+void parser(int argc, char **argv, opt *options) {
     int opt;
     int option_index;
-    while ((opt = getopt_long(argc, argv, "+benstvTE", long_options, &option_index)) != -1)
-    {
-        if (opt == 'e')
-        {
+    while ((opt = getopt_long(argc, argv, "+benstvTE", long_options, &option_index)) != -1) {
+        if (opt == 'e') {
             options->e = 1;
             options->v = 1;
         }
-        else if (opt == 't')
-        {
+        if (opt == 't') {
             options->t = 1;
             options->v = 1;
         }
-        else if (opt == 'b')
+        if (opt == 'b')
             options->b = 1;
-        else if (opt == 'n')
+        if (opt == 'n')
             options->n = 1;
-        else if (opt == 's')
+        if (opt == 's')
             options->s = 1;
-        else  if (opt == 'v')
+        if (opt == 'v')
             options->v = 1;
-        else if (opt == 'T')
+        if (opt == 'T')
             options->t = 1;
-        else if (opt == 'E')
+        if (opt == 'E') {
             options->e = 1;
-        else
-        {
+        } else {
             fprintf(stderr, "use benstvTE");
             exit(1);
         }
     }
 }
 
-void reader(char **argv, opt *options)
-{
+void reader(char **argv, opt *options) {
     FILE *fp;
     fp = fopen(argv[optind], "r");
-    if (fp)
-    {
-        int cur = 0;
-        int counter = 0;
-        int str_count = 0;
-        while ((cur = fgetc(fp)) != EOF)
-        {
-            if (options->b == 1)
-            {
-                if (cur != '\n')
-                {
-                    if (counter == 0)
-                    {
-                        printf("%6d\t", ++str_count);
-                        counter = 1;
-                    }
-                }
-                else
-                    counter = 0;
+    if (fp) {
+        int str_count = 1;
+        int empty_str = 0;
+        int last_sym = '\n';
+        while (1) {
+            int cur = fgetc(fp);
+            if (cur == EOF)
+                break;
+            if (options->s && cur == '\n' && last_sym == '\n') {
+                empty_str++;
+                if (empty_str > 1)
+                    continue;
+            } else {
+                empty_str = 0;
             }
-            if (options->n == 1)
-            {
-                if (cur != '\n')
-                {
-                    
+            if (last_sym == '\n' && ((options->b && cur != '\n') || options->n))
+                printf("%6d\t", str_count++);
+            if (options->e == 1 && cur == '\n')
+                printf("$");
+            if (options->t && cur == '\t') {
+                printf("^");
+                cur = 'I';
+            }
+            if (options->v) {
+                if ((cur >= 0 && cur < 9) || (cur > 10 && cur < 32)) {
+                    printf("^");
+                    cur += 64;
                 }
-                else
-                    counter = 0;
             }
         printf("%c", cur);
+        last_sym = cur;
         }
-    }
-    else
+    fclose(fp);
+    } else {
         printf("No such file");
+    }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     opt options = {0};
 
-    if (argc < 2)
-    {
+    if (argc < 2) {
         printf("Неверное колличество аргументов");
         exit(1);
     }
@@ -108,6 +100,10 @@ int main(int argc, char **argv)
     parser(argc, argv, &options);
     if (options.b)
         options.n = 0;
+    if (options.v) {
+        options.t = 0;
+        options.e = 0;
+    }
     reader(argv, &options);
 
     return (0);
